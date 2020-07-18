6d1f702eace30df30083a44da74f6637cf5880fd
Qijun Yang <Qijun.Yang@morningstar.com>
2020-07-17 19:46:48 -0500

    delete unused files

--- packages/awsamplifytutorial/__tests__/awsamplifytutorial.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const awsamplifytutorial = require('..');
-
-describe('awsamplifytutorial', () => {
-    it('needs tests');
-});
--- packages/awsamplifytutorial/lib/awsamplifytutorial.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = awsamplifytutorial;
-
-function awsamplifytutorial() {
-    // TODO
-}
